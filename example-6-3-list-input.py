4d9a22dbb459999b79ba987f0d53fdd85aef2a1f
Glen-Sasek-PCC-Student <glen.sasek+student@pcc.edu>
2026-02-09 23:05:35 +0000

    Add

---
+++ example-6-3-list-input.py
@@ -0,0 +1,19 @@
+#reads colors from the user and appends to a list
+#until the user presses the Enter key.
+def main():
+  colors = []
+  #notice how we read one time outside the loop
+  #check and then enter the loop
+  aColor = input("Enter a color: ")
+  while (aColor != ''):
+    colors.append(aColor)
+    aColor = input("Enter a color: ")
+
+  print(colors)
+  print("If you want it formatted better: ")
+  for num in range(len(colors)-1):
+    print(colors[num], ", ", sep = '', end = '')
+  print(colors[num+1])
+
+#call main
+main()
